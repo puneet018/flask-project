1c0a44d93aa4af098e77e0d659929e098882f473
puneet vishwakarma <nikkyvishwa90@gmail.com>
2024-05-17 07:41:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+import json
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
 
+PORT = 8000
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -12,9 +16,97 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+class API():
+    def __init__(self):
+        self.routing = { "GET": { }, "POST": { } }
+    
+    def get(self, path):
+        def wrapper(fn):
+            self.routing["GET"][path] = fn
+        return wrapper
+
+    def post(self, path):
+        def wrapper(fn):
+            self.routing["POST"][path] = fn
+        return wrapper
+
+api = API()
+
+example_data = {
+    "items": [
+        { "id": 1000, "name": "cat", "description": "cat is meowing" },
+        { "id": 1001, "name": "dog", "description": "dog is barking" },
+        { "id": 1002, "name": "bird", "description": "bird is singing" }
+    ]
+}
+
+@api.get("/")
+def index(_):
+    return { 
+        "name": "Python REST API Example",
+        "summary": "This is simple REST API architecture with pure Python",
+        "actions": [ "add", "delete", "list", "search" ],
+        "version": "1.0.0"
+    }
+
+
+@api.get("/list")
+def list(_):
+    return {
+        "count": len(example_data["items"]),
+        "items": example_data["items"]
+    }
+
 # main driver function
 if __name__ == '__main__':
+     class ApiRequestHandler(BaseHTTPRequestHandler):
+        global api
+        
+        def call_api(self, method, path, args):
+            if path in api.routing[method]:
+                try:
+                    result = api.routing[method][path](args)
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(json.dumps(result, indent=4).encode())
+                except Exception as e:
+                    self.send_response(500, "Server Error")
+                    self.end_headers()
+                    self.wfile.write(json.dumps({ "error": e.args }, indent=4).encode())
+            else:
+                self.send_response(404, "Not Found")
+                self.end_headers()
+                self.wfile.write(json.dumps({"error": "not found"}, indent=4).encode())
 
+        def do_GET(self):
+            parsed_url = urlparse(self.path)
+            path = parsed_url.path
+            args = parse_qs(parsed_url.query)
+            
+            for k in args.keys():
+                if len(args[k]) == 1:
+                    args[k] = args[k][0]
+            
+            self.call_api("GET", path, args)
+
+        def do_POST(self):
+            parsed_url = urlparse(self.path)
+            path = parsed_url.path
+            if self.headers.get("content-type") != "application/json":
+                self.send_response(400)
+                self.end_headers()
+                self.wfile.write(json.dumps({
+                    "error": "posted data must be in json format"
+                }, indent=4).encode())
+            else:
+                data_len = int(self.headers.get("content-length"))
+                data = self.rfile.read(data_len).decode()
+                self.call_api("POST", path, json.loads(data))
+
+
+httpd = HTTPServer(('', PORT), ApiRequestHandler)
+print(f"Application started at http://127.0.0.1:{PORT}/")
+httpd.serve_forever()
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    # app.run()
