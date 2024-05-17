70739f9d31ed5293202eadc3fa6c970b9f3c8fcd
puneet vishwakarma <nikkyvishwa90@gmail.com>
2024-05-17 08:21:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,42 +11,42 @@ data = [
 # Route to get all users
 @app.route('/users', methods=['GET'])
 def get_users():
-    return jsonify(data)
+    return data
 
 # Route to get a user by ID
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = next((u for u in data if u['id'] == user_id), None)
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({'error': 'User not found'}), 404
+# @app.route('/users/<int:user_id>', methods=['GET'])
+# def get_user(user_id):
+#     user = next((u for u in data if u['id'] == user_id), None)
+#     if user:
+#         return jsonify(user)
+#     else:
+#         return jsonify({'error': 'User not found'}), 404
 
-# Route to create a new user
-@app.route('/users', methods=['POST'])
-def create_user():
-    new_user = request.get_json()
-    new_user['id'] = len(data) + 1
-    data.append(new_user)
-    return jsonify(new_user), 201
+# # Route to create a new user
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     new_user = request.get_json()
+#     new_user['id'] = len(data) + 1
+#     data.append(new_user)
+#     return jsonify(new_user), 201
 
-# Route to update a user by ID
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = next((u for u in data if u['id'] == user_id), None)
-    if user:
-        updates = request.get_json()
-        user.update(updates)
-        return jsonify(user)
-    else:
-        return jsonify({'error': 'User not found'}), 404
+# # Route to update a user by ID
+# @app.route('/users/<int:user_id>', methods=['PUT'])
+# def update_user(user_id):
+#     user = next((u for u in data if u['id'] == user_id), None)
+#     if user:
+#         updates = request.get_json()
+#         user.update(updates)
+#         return jsonify(user)
+#     else:
+#         return jsonify({'error': 'User not found'}), 404
 
-# Route to delete a user by ID
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    global data
-    data = [u for u in data if u['id'] != user_id]
-    return '', 204
+# # Route to delete a user by ID
+# @app.route('/users/<int:user_id>', methods=['DELETE'])
+# def delete_user(user_id):
+#     global data
+#     data = [u for u in data if u['id'] != user_id]
+#     return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True)
