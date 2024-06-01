f216a3e673dcb22088b5c0b7084aeced7e4d74d7
puneet vishwakarma <nikkyvishwa90@gmail.com>
2024-06-01 08:36:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, session
 from flask_cors import CORS, cross_origin
 from flask_pymongo import PyMongo , pymongo
+from twilio.rest import Client
+from dotenv import load_dotenv
+from jsonify import convert
+import os
 
 app = Flask(__name__)
 # app.config["MONGO_URI"] = "mongodb+srv://nikkyvishwa90:nikkyvishwa90@cluster0.jc8u7cz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/sample_mflix"
@@ -19,14 +23,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # 	for updated, _ in results:
 # 		yield str(counter).decode('utf-8')
             
-data = [
-{'id': 1, 'name': 'Shivam', 'age': 30},
-{'id': 2, 'name': 'Puneet', 'age': 25},
-{'id': 3, 'name': 'Ankit', 'age': 40},
-{'id': 4, 'name': 'Akash', 'age': 20},
-{'id': 5, 'name': 'Harsit', 'age': 30},
-{'id': 6, 'name': 'Vineet', 'age': 25},
-]
 
 client = pymongo.MongoClient('mongodb+srv://shivams:shivams@cluster0.jc8u7cz.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0')
 userdb = client['FlaskDB']
@@ -42,23 +38,7 @@ def get_users():
 @cross_origin()
 def insert_data():
     
-	# if request.method == 'POST':
 		data = request.get_json()
-		# name = request.form['name']
-		# email = request.form['email']
-		# password = request.form['password']
-       
-		# reg_user = {}
-		# reg_user['name'] = name
-		# reg_user['email'] = email
-		# reg_user['password'] = password
-    
-        # parser = reqparse.RequestParser()
-        # parser.add_argument('title', required=True)
-        # parser.add_argument('author', required=True)
-        # parser.add_argument('published_date', required=True)
-        #     args = parser.parse_args()
-
 		# if name != '':
 		_id = users.insert_one(data)
 		print(_id.inserted_id)
@@ -66,6 +46,62 @@ def insert_data():
 		# else:
 		# 	return "False"
 
+def check_user():
+
+	if request.method == 'POST':
+		user = request.get_json()
+
+		user_data = users.find_one(user)
+		if user_data == None:
+			return False, ""
+		else:
+			return True, user_data["name"]
+
+
+# OTP authentication confrigration
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+verify_sid = os.getenv("VERIFY_SID")
+from_number = os.getenv("TWILIO_NUMBER")
+client = Client(account_sid,auth_token)
+
+# # Route to create a new user
+@app.route('/create_user', methods=['POST'])
+def create_user():
+	new_user = request.get_json()
+	# if name != '':
+	_id = users.insert_one(new_user)
+	number = new_user['number']
+	send_otp_via_sms(number)
+    # data.append(new_user)
+	return 'otp send'
+
+
+def send_otp_via_sms(number):
+	# massage = client.messages.create(body="Hello world", from_=from_number,
+	#   to=number)
+	
+	otp_verification = client.verify.services(verify_sid).verifications.create(
+	 to=number, channel="sms")
+	print(otp_verification.status)
+	# print(messages.status['valid'])
+	# print(messages.status.valid)
+	# print(messages['valid'])
+	# print(messages.valid)
+
+    # messages = client.messages.create(to=f"{number}", from_=os.getenv(
+    #     'TWILIO_NUMBER'), body=f"Your one-time password is {code}")
+# input_otp = input("enter")
+# check_opt(input_otp)
+@app.route('/check_otp', methods=['POST'])
+def check_otp():
+	verify_data = request.get_json()
+	print(verify_data['number'])
+	otp_check = client.verify.services(verify_sid).verification_checks.create(
+		to=verify_data['number'], code=verify_data['otp_code']
+	)
+	print(otp_check.status)
+	return 'done'
 # Route to get a user by ID
 # @app.route('/users/<int:user_id>', methods=['GET'])
 # def get_user(user_id):
@@ -75,13 +111,6 @@ def insert_data():
 #     else:
 #         return jsonify({'error': 'User not found'}), 404
 
-# # Route to create a new user
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     new_user = request.get_json()
-#     new_user['id'] = len(data) + 1
-#     data.append(new_user)
-#     return jsonify(new_user), 201
 
 # # Route to update a user by ID
 # @app.route('/users/<int:user_id>', methods=['PUT'])
